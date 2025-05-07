fab5de99650ea532f165a3d7d26547f0abd4326a
Luis Antonio <tacomusical@gmail.com>
2025-05-07 11:44:10 -0600

    Add files via upload

---
+++ AllArcheologicalSitesOfMexico/src/main/java/com/example/AllArcheologicalSitesOfMexico/AllArcheologicalSitesOfMexicoApplication.java
@@ -0,0 +1,13 @@
+package com.example.AllArcheologicalSitesOfMexico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllArcheologicalSitesOfMexicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AllArcheologicalSitesOfMexicoApplication.class, args);
+	}
+
+}
---
+++ AllArcheologicalSitesOfMexico/src/test/java/com/example/AllArcheologicalSitesOfMexico/AllArcheologicalSitesOfMexicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AllArcheologicalSitesOfMexico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllArcheologicalSitesOfMexicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
